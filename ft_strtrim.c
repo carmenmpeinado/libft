f8e6744c2323c526f66f2d0b340e4fc502b38813
carmen@cachyos <carmenmariapa@gmail.com>
2025-10-15 23:49:02 +0200

    añadido strtrim

---
+++ ft_strtrim.c
@@ -0,0 +1,26 @@
+#include "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	start;
+	size_t	end;
+
+	if (!s1 || !set)
+		return (0);
+	start = 0;
+	while (s1[start] && ft_strchr(set, s1[start]))
+		start++;
+	end = ft_strlen(s1);
+	while (end > start && ft_strchr(set, s1[end - 1]))
+		end--;
+	return (ft_substr(s1, start, end - start));
+}
+// #include <stdio.h>
+	
+// int main(void)
+// {
+// 	const char *s1 = "LOLintentoLOL";
+// 	const char *s2 = "LOL";
+// 	printf("%s", ft_strtrim(s1, s2));
+
+// }
